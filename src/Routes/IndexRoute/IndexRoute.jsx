50026c7810ca420b89e4e3c989df186f52e7aecb
Jonathan Dump <jbrodii77@gmail.com>
2024-02-14 20:52:43 +0200

    +Search

--- src/Routes/IndexRoute/IndexRoute.jsx
+++ src/Routes/IndexRoute/IndexRoute.jsx
@@ -3,13 +3,21 @@ import cl from "./IndexRoute.module.scss";
 import { useSelector } from "react-redux";
 import { selectTasks } from "../../redux/slices/tasks";
 import Task from "../../components/task/Task";
+import Search from "../../components/search/Search";
+import { useEffect, useState } from "react";
 
 function IndexRoute() {
-  const tasks = useSelector(selectTasks);
-  console.log(tasks);
+  const allTasks = useSelector(selectTasks);
+
+  const [tasks, setTasks] = useState(allTasks);
+
+  useEffect(() => {
+    setTasks(allTasks);
+  }, [allTasks]);
 
   return (
     <div className={cl.indexRoute}>
+      <Search tasks={allTasks} setTasks={setTasks} />
       <FormCreateTask />
       {tasks.map((task) => (
         <Task {...task} key={task._id} />
---
+++ src/components/search/Search.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import cl from "./Search.module.scss";
+
+const Search = ({ tasks, setTasks }) => {
+  const [formState, setFormState] = useState({ filter: "name", input: "" });
+  let timer;
+
+  const handleChange = (e) => {
+    setFormState((prev) => {
+      const updated = { ...prev, [e.target.name]: e.target.value };
+
+      clearTimeout(timer);
+
+      timer = setTimeout(() => {
+        console.log("searching");
+        searchTasks({ tasks, setTasks, formState: updated });
+      }, 500);
+
+      return updated;
+    });
+  };
+
+  return (
+    <div className={cl.search}>
+      <form action="">
+        <div className={cl.inputContainer}>
+          <label htmlFor="filter">Search by</label>
+          <select
+            name="filter"
+            id="filter"
+            value={formState.filter}
+            onChange={handleChange}
+          >
+            <option value="name">Name</option>
+            <option value="createdBy">Created By</option>
+            <option value="deadline">Deadline</option>
+          </select>
+        </div>
+
+        <input
+          type="text"
+          name="input"
+          value={formState.input}
+          onChange={handleChange}
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Search;
+
+function searchTasks({ tasks, setTasks, formState }) {
+  const { input, filter } = formState;
+
+  setTasks(tasks.filter((task) => task[filter].includes(input)));
+}
